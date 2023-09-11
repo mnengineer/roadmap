4fd2788d48ccce3cb301f26e07e6f55d82c6ef4c
mnengineer <masashiii0115@gmail.com>
2023-09-11 23:09:57 +0900

    add: Color定義ファイル作成

---
+++ lib/presentation/theme/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/* -- LIST OF ALL COLORS -- */
+
+const tPrimaryColor = Color(0xFFFFE400);
+const tSecondaryColor = Color(0xFF272727);
+const tAccentColor = Color(0xFF001BFF);
+
+const tWhiteColor = Colors.white;
+const tDarkColor = Color(0xff272727);
+const tCardBgColor = Color(0xFFF7F5F1);
+
+// -- SOCIAL Button COLORS
+const tGoogleBgColor = Color(0xFFDFEFFF);
+const tGoogleForegroundColor = Color(0xFF167EE6);
+const tFacebookBgColor = Color(0xFF1877F2);
+
+// -- ON-BOARDING COLORS
+const tOnBoardingPage1Color = Colors.white;
+const tOnBoardingPage2Color = Color(0xfffddcdf);
+const tOnBoardingPage3Color = Color(0xffffdcbd);
