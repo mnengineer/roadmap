f8c8da661a98193388eb10ebc37790521d92e77e
mnengineer <masashiii0115@gmail.com>
2023-09-11 23:14:20 +0900

    add: Size定義ファイル作成

---
+++ lib/presentation/theme/sizes.dart
@@ -0,0 +1,10 @@
+//App default Sizing
+const tDefaultSpace = 20.0;
+const tSplashContainerSize = 30.0;
+const tButtonHeight = 20.0;
+const tFormHeight = 30.0;
+const tBorderRadius = 15.0;
+
+//Dashboard
+const tDashboardPadding = 20.0;
+const tDashboardCardPadding = 10.0;
