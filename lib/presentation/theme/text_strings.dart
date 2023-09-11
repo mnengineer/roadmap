d6e56d2820df42370ea486c546dfa67433013920
mnengineer <masashiii0115@gmail.com>
2023-09-11 23:19:19 +0900

    add: テキスト定義ファイル作成

---
+++ lib/presentation/theme/text_strings.dart
@@ -0,0 +1,108 @@
+/* -- App Text Strings -- */
+
+// -- GLOBAL Texts
+const String tNo = 'No';
+const String tYes = 'Yes';
+const String tNext = 'Next';
+const String tLogin = 'Login';
+const String tEmail = 'E-Mail';
+const String tSignup = 'Signup';
+const String tLogout = 'Logout';
+const String tSuccess = 'Success';
+const String tPhoneNo = 'Phone No';
+const String tContinue = 'Continue';
+const String tPassword = 'Password';
+const String tFullName = 'Full Name';
+const String tGetStarted = 'Get Started';
+const String tForgetPassword = 'Forget Password?';
+const String tSignInWithGoogle = 'Sign-In with Google';
+
+// -- Validation --
+const String tEmailCannotEmpty = 'Email cannot be empty';
+const String tInvalidEmailFormat = 'Invalid email format';
+const String tNoRecordFound = 'No record found';
+
+// -- SnackBar --
+const String tAlert = 'Alert';
+const String tOhSnap = 'Oh Snap';
+const String tEmailSent = 'Hurray!!! Email is on its way.';
+const String tCongratulations = 'Congratulations';
+const String tEmailLinkToResetPassword = 'Email Link To Reset Password';
+const String tAccountCreateVerifyEmail = 'Account Create Verify Email';
+
+// -- Splash Screen Text
+const String tAppName = 'Road Map:';
+const String tAppTagLine = 'road';
+
+// -- On Boarding Text
+const String tOnBoardingTitle1 = 'Build Awesome Apps';
+const String tOnBoardingTitle2 = 'Learn from YouTube';
+const String tOnBoardingTitle3 = 'Get Code & Resources';
+const String tOnBoardingSubTitle1 = '1';
+const String tOnBoardingSubTitle2 = '2';
+const String tOnBoardingSubTitle3 = '3';
+const String tOnBoardingCounter1 = '1/3';
+const String tOnBoardingCounter2 = '2/3';
+const String tOnBoardingCounter3 = '3/3';
+
+// -- Welcome Screen Text
+const String tWelcomeTitle = 'Build Awesome Apps';
+const String tWelcomeSubTitle =
+    "Let's put your creativity on the development highway.";
+
+// -- Login Screen Text
+const String tLoginTitle = 'Welcome Back,';
+const String tLoginSubTitle = 'Make it work, make it right, make it fast.';
+const String tRememberMe = 'Remember Me?';
+const String tDontHaveAnAccount = "Don't have an Account";
+const String tEnterYour = 'Enter your';
+const String tResetPassword = 'Reset Password';
+const String tOR = 'OR';
+const String tConnectWith = 'Connect With';
+const String tFacebook = 'Facebook';
+const String tGoogle = 'Google';
+
+// -- Sign Up Screen Text
+const String tSignUpTitle = 'Get On Board!';
+const String tSignUpSubTitle = 'Create your profile to start your Journey.';
+const String tAlreadyHaveAnAccount = 'Already have an Account';
+
+// -- Forget Password Text
+const String tForgetPasswordTitle = 'Make Selection!';
+const String tForgetPasswordSubTitle =
+    'Select one of the options given below to reset your password.';
+const String tResetViaEMail = 'Reset via Mail Verification';
+const String tResetViaPhone = 'Reset via Phone Verification';
+
+// -- Forget Password Via Phone - Text
+const String tForgetPhoneSubTitle =
+    'Enter your registered Phone No to receive OTP';
+
+// -- Forget Password Via E-Mail - Text
+const String tForgetMailSubTitle =
+    'Enter your registered E-Mail to receive OTP';
+
+// -- OTP Screen - Text
+const String tOtpTitle = 'CO\nDE';
+const String tOtpSubTitle = 'Verification';
+const String tOtpMessage = 'Enter the verification code sent at ';
+
+// -- Email Verification
+const String tEmailVerificationTitle = 'Verify your email address';
+const String tEmailVerificationSubTitle =
+    // ignore: prefer_interpolation_to_compose_strings, prefer_adjacent_string_concatenation
+    'We have just send email verification link on your email. Please check' +
+        'email and click on that link to verify your Email address. \n\n If' +
+        'not auto redirected after verification, click on the Continue button.';
+const String tResendEmailLink = 'Resend E-Mail Link';
+const String tBackToLogin = 'Back to login';
+
+// -- Profile Screen - Text
+const String tProfile = 'Profile';
+const String tEditProfile = 'Edit Profile';
+const String tLogoutDialogHeading = 'Logout';
+
+// -- Update Profile Screen - Text
+const String tDelete = 'Delete';
+const String tJoined = 'Joined';
+const String tJoinedAt = ' 31 October 2022';
