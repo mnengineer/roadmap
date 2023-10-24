f9bb17bcc7513f063bfaf62c44c3e970056b2dc6
mnengineer <masashiii0115@gmail.com>
2023-10-25 03:04:44 +0900

    🚀 feat: ボトムナビゲーションバー実装

---
+++ lib/presentation/views/tabs/stats_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StatsTab extends StatelessWidget {
+  const StatsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ここに統計タブの内容を追加
+    return const Center(child: Text('統計'));
+  }
+}
