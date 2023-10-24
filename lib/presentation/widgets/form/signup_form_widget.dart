4b84376ea69c9bcb466e692d89a9b8f573f262e8
mnengineer <masashiii0115@gmail.com>
2023-10-25 00:48:57 +0900

    💡 refactor: コメント修正

--- lib/presentation/widgets/form/signup_form_widget.dart
+++ lib/presentation/widgets/form/signup_form_widget.dart
@@ -57,7 +57,7 @@ class SignUpFormWidget extends HookConsumerWidget {
             ),
             const SizedBox(height: tFormHeight - 20),
 
-            /// -- SignUp BTN
+            /// -- SIGNUP BTN
             TPrimaryButton(
               isLoading: state.isLoading,
               text: tSignup,
