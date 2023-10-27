d9fd2c3d33dffd7f49b2486892fae88eab296f15
mnengineer <masashiii0115@gmail.com>
2023-10-27 14:29:01 +0900

    🚀 feat: ホーム画面 ListTileのUI実装

--- lib/presentation/views/tabs/home_tab.dart
+++ lib/presentation/views/tabs/home_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:roadmap/core/di/providers.dart';
 import 'package:roadmap/core/utils/date_utils.dart';
+import 'package:roadmap/presentation/views/mypage_screen.dart';
 import 'package:roadmap/presentation/widgets/add_item_dialog.dart';
 
 class HomeTab extends HookConsumerWidget {
@@ -13,61 +14,154 @@ class HomeTab extends HookConsumerWidget {
     final state = ref.watch(homeTabViewModelProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Roadmap')),
-      body: state.when(
-        data: (items) => items.isEmpty
-            ? const Center(
-                child: Text(
-                  'タスクがありません',
-                  style: TextStyle(fontSize: 20),
+      appBar: AppBar(
+        title: Text(
+          'Roadmap',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.grey[900],
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet<void>(
+                context: context,
+                isScrollControlled: true,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
                 ),
-              )
-            : ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final item = items[index];
-
-                  return ProviderScope(
-                    child: Dismissible(
-                      key: ValueKey(item.id),
-                      background: Container(
-                        color: Colors.red,
-                      ),
-                      onDismissed: (_) {
-                        viewModel.deleteItem(
-                          itemId: item.id!,
-                        );
-                      },
-                      child: Column(
+                builder: (context) {
+                  final height = MediaQuery.of(context).size.height;
+                  final desiredHeight = height * 0.9;
+                  return SizedBox(
+                    height: desiredHeight,
+                    child: const MyPageScreen(),
+                  );
+                },
+              );
+            },
+            icon: const Icon(Icons.person_outline),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/dashboard/3.jpeg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          state.when(
+            data: (items) => items.isEmpty
+                ? const Center(
+                    child: Text(
+                      'タスクがありません',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = items[index];
+                      return Column(
                         children: [
                           ListTile(
-                            key: ValueKey(item.id),
-                            title: Text(item.title),
-                            subtitle: Text(
-                              formatToJapaneseDate(item.createdAt),
-                            ),
-                            trailing: Checkbox(
-                              value: item.isCompleted,
-                              onChanged: (_) => viewModel.updateItem(
-                                updatedItem: item.copyWith(
-                                  isCompleted: !item.isCompleted,
-                                ),
-                              ),
-                            ),
+                            title: item.title,
+                            description:
+                                '期日：${formatToJapaneseDate(item.createdAt)}',
+                            progress: 50,
+                            imagePath: 'assets/images/dashboard/34.jpg',
                             onTap: () => AddItemDialog.show(context, item),
-                            onLongPress: () => viewModel.deleteItem(
-                              itemId: item.id!,
-                            ),
                           ),
                           const Divider(height: 2),
                         ],
-                      ),
-                    ),
-                  );
-                },
+                      );
+                    },
+                  ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, _) => Text(error.toString()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ListTile extends StatelessWidget {
+  const ListTile({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.progress,
+    required this.imagePath,
+    required this.onTap,
+  });
+  final String title;
+  final String description;
+  final int progress;
+  final String imagePath;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imagePath),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, _) => Text(error.toString()),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              description,
+              style: const TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: LinearProgressIndicator(
+                    value: progress / 100,
+                    backgroundColor: Colors.grey,
+                    valueColor: const AlwaysStoppedAnimation<Color>(
+                      Colors.yellow,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  '$progress%',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
