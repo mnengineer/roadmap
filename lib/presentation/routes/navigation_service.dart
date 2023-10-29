7ee9dce4d3d573afc36d20cf0e238406a5c58154
mnengineer <masashiii0115@gmail.com>
2023-10-29 22:32:49 +0900

    💡 refactor: ナビゲーション構造のリファクタリング及びルート定数の追加

--- lib/presentation/routes/navigation_service.dart
+++ lib/presentation/routes/navigation_service.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:roadmap/domain/entities/item.dart';
+import 'package:roadmap/core/constants/routes.dart';
 
 class NavigationService {
   NavigationService(this.router);
@@ -10,35 +10,17 @@ class NavigationService {
     router.pop();
   }
 
-  void navigateToOnBoarding() {
-    router.go('/onboarding');
+  void navigateTo(String path, {Object? extra}) {
+    router.go(path, extra: extra);
   }
 
-  void navigateToWelcome() {
-    router.go('/welcome');
-  }
-
-  void navigateToLogin() {
-    router.go('/login');
-  }
-
-  void navigateToSignup() {
-    router.go('/signup');
-  }
-
-  void navigateToHome() {
-    router.go('/home');
-  }
-
-  void navigateToAdd() {
-    router.push('/add');
-  }
-
-  void navigateToEdit(Item item) {
-    router.push('/edit', extra: item);
-  }
-
-  void navigateToDetail(Item item) {
-    router.push('/detail', extra: item);
-  }
+  void navigateToOnBoarding() => navigateTo(Routes.onboarding);
+  void navigateToWelcome() => navigateTo(Routes.welcome);
+  void navigateToLogin() => navigateTo(Routes.login);
+  void navigateToSignup() => navigateTo(Routes.signup);
+  void navigateToHome() => navigateTo(Routes.home);
+  void navigateToAdd() => navigateTo(Routes.add);
+  void navigateToEdit(Object extra) => navigateTo(Routes.edit, extra: extra);
+  void navigateToDetail(Object extra) =>
+      navigateTo(Routes.detail, extra: extra);
 }
