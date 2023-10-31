44d1efa7185def0fdc9a45e2b90a5de455dd79e4
mnengineer <masashiii0115@gmail.com>
2023-10-31 14:28:29 +0900

    🚀 feat: 共通のエラーダイアログ作成

--- lib/presentation/widgets/dialog/error_dialog.dart
+++ lib/presentation/widgets/dialog/error_dialog.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ErrorDialog extends StatelessWidget {
-  const ErrorDialog({super.key, required this.message});
-  final String message;
+final errorDialogProvider = Provider((ref) => ErrorDialog());
 
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Error'),
-      content: Text(message),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text('OK'),
-        ),
-      ],
+class ErrorDialog {
+  void showErrorDialog(
+    BuildContext context, {
+    required String title,
+    required String message,
+  }) {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
