5d1d03b472326fba7626500ceee5777e6a09b978
mnengineer <masashiii0115@gmail.com>
2023-11-26 10:18:18 +0900

    💡 refactor: ロードマップ追加・編集モーダルを別ファイルに抽出

--- lib/presentation/views/home/goal/detail_screen.dart
+++ lib/presentation/views/home/goal/detail_screen.dart
@@ -4,7 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:roadmap/core/di/providers.dart';
 import 'package:roadmap/domain/entities/goal_item.dart';
-import 'package:roadmap/domain/entities/roadmap_item.dart';
+import 'package:roadmap/presentation/widgets/modal/roadmap_add_modal_bottom_sheet.dart';
+import 'package:roadmap/presentation/widgets/modal/roadmap_edit_modal_bottom_sheet.dart';
 import 'package:roadmap/presentation/widgets/tile/roadmap_tile.dart';
 
 class DetailScreen extends HookConsumerWidget {
@@ -25,300 +26,6 @@ class DetailScreen extends HookConsumerWidget {
       },
       const [],
     );
-
-    Future<void> selectDate(BuildContext dialogContext) async {
-      final picked = await showDatePicker(
-        context: dialogContext,
-        initialDate: selectedDate.value,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2101),
-      );
-      if (picked != null && picked != selectedDate.value) {
-        selectedDate.value = picked;
-      }
-    }
-
-    Future<void> showAddModalBottomSheet() async {
-      var title = '';
-      var description = '';
-      selectedDate.value = DateTime.now();
-
-      await showModalBottomSheet<void>(
-        context: context,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-        ),
-        builder: (BuildContext context) {
-          return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Add Roadmap Item',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    TextField(
-                      onChanged: (value) => title = value,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Title',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      onChanged: (value) => description = value,
-                      minLines: 3,
-                      maxLines: 5,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Description',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Deadline',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton.icon(
-                      icon: const Icon(Icons.calendar_today),
-                      label: Text(
-                        DateFormat('yyyy-MM-dd').format(selectedDate.value),
-                      ),
-                      onPressed: () async {
-                        await selectDate(context);
-                        setState(() {});
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: Theme.of(context).primaryColor,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 20,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          side: BorderSide(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: viewModel.navigatePop,
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.black87,
-                            backgroundColor: Colors.grey[300],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 32,
-                              vertical: 16,
-                            ),
-                          ),
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            viewModel
-                              ..addRoadmapItem(
-                                itemId: item.id!,
-                                title: title,
-                                description: description,
-                                deadline: selectedDate.value,
-                              )
-                              ..navigatePop();
-                          },
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Theme.of(context).primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 32,
-                              vertical: 16,
-                            ),
-                          ),
-                          child: const Text('Add'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      );
-    }
-
-    Future<void> showEditModalBottomSheet({
-      required String roadmapItemId,
-      required String title,
-      required String description,
-      required DateTime deadline,
-      required bool isCompleted,
-    }) async {
-      final titleController = TextEditingController(text: title);
-      final descriptionController = TextEditingController(text: description);
-      selectedDate.value = deadline;
-
-      await showModalBottomSheet<void>(
-        context: context,
-        isScrollControlled: true,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-        ),
-        builder: (BuildContext context) {
-          return StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Edit Roadmap Item',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    TextField(
-                      controller: titleController,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Title',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: descriptionController,
-                      minLines: 3,
-                      maxLines: 5,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Description',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Deadline',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextButton.icon(
-                      icon: const Icon(Icons.calendar_today),
-                      label: Text(
-                        DateFormat('yyyy-MM-dd').format(selectedDate.value),
-                      ),
-                      onPressed: () async {
-                        await selectDate(context);
-                        setState(() {});
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: Theme.of(context).primaryColor,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 20,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          side: BorderSide(
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: viewModel.navigatePop,
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.black87,
-                            backgroundColor: Colors.grey[300],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 32,
-                              vertical: 16,
-                            ),
-                          ),
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            viewModel
-                              ..updateRoadmapItem(
-                                goalItemId: roadmapItemId,
-                                updatedItem: RoadmapItem(
-                                  id: roadmapItemId,
-                                  title: titleController.text,
-                                  description: descriptionController.text,
-                                  deadline: selectedDate.value,
-                                  isCompleted: isCompleted,
-                                  createdAt: DateTime.now(),
-                                ),
-                              )
-                              ..navigatePop();
-                          },
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Theme.of(context).primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 32,
-                              vertical: 16,
-                            ),
-                          ),
-                          child: const Text('Update'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      );
-    }
 
     Future<void> showDeleteRoadmapItemDialog({
       required String itemId,
@@ -418,7 +125,11 @@ class DetailScreen extends HookConsumerWidget {
                       isFirst: roadmapItems.first == roadmapItem,
                       isLast: roadmapItems.last == roadmapItem,
                       onEdit: () {
-                        showEditModalBottomSheet(
+                        showRoadmapEditModalBottomSheet(
+                          context: context,
+                          ref: ref,
+                          selectedDate: selectedDate,
+                          item: item,
                           roadmapItemId: roadmapItem.id!,
                           title: roadmapItem.title,
                           description: roadmapItem.description,
@@ -443,7 +154,12 @@ class DetailScreen extends HookConsumerWidget {
                     ),
                   const SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: showAddModalBottomSheet,
+                    onPressed: () => showRoadmapAddModalBottomSheet(
+                      context: context,
+                      ref: ref,
+                      selectedDate: selectedDate,
+                      item: item,
+                    ),
                     child: const Text('Add Roadmap Item'),
                   ),
                 ],
